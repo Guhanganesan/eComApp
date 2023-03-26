6b9ec6d0104c7643cb2359b1892606538f22d1f7
Guhan Ganesan <phpguhan@gmail.com>
2023-03-26 23:20:57 +0530

    render data in checkout page

--- E_Com_Web/myapp/views.py
+++ E_Com_Web/myapp/views.py
@@ -12,11 +12,9 @@ def cart(request):
     print(request.user)
     if request.user.is_authenticated:
         customer = request.user.customer
-        print(customer)
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        print(order)
         items = order.orderitem_set.all()
-        print(items)
+        
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
@@ -25,6 +23,13 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
-    context = {}
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
