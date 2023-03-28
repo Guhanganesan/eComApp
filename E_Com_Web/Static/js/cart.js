58ad78ecd719f12bcc888cff9de612b4dbd25c0b
Guhan Ganesan <phpguhan@gmail.com>
2023-03-28 23:09:31 +0530

    testing a logged in user

--- E_Com_Web/Static/js/cart.js
+++ E_Com_Web/Static/js/cart.js
@@ -7,5 +7,13 @@ for(var i=0; i<updatebtns.length; i++){
         var product_id = this.dataset.product;
         var action = this.dataset.action;
         console.log('Product Id: ', product_id, 'Action', action);
+
+        console.log('user:', user);
+        if(user == 'guhan_test@gmail.com'){
+            console.log('user is authenticated')
+        }else{
+            console.log('user is not authenticated')
+        }
+
     })
 }
