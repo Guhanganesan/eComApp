b02d65c04135af5ecd98d72158b67834da2d9d6d
Guhan Ganesan <phpguhan@gmail.com>
2023-03-28 23:34:45 +0530

    example for adding csrf token with ajax query in django

--- E_Com_Web/Static/js/cart.js
+++ E_Com_Web/Static/js/cart.js
@@ -10,10 +10,28 @@ for(var i=0; i<updatebtns.length; i++){
 
         console.log('user:', user);
         if(user == 'guhan_test@gmail.com'){
-            console.log('user is authenticated')
+            // console.log('user is authenticated')
+            updateUserOrder(product_id, action);
         }else{
             console.log('user is not authenticated')
         }
 
+    })
+}
+
+function updateUserOrder(product_id, action){
+    console.log('User is authenticated');
+
+    var url = '/update_item/'; // view.py file 
+    fetch(
+        url, 
+        {
+            method:'POST', headers:{'Content-Type':'Application/json', 'X-CSRFToken':csrftoken},
+            body:JSON.stringify({'product_id':product_id, 'action':action})
+        }
+    ).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
     })
 }
